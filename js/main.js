a2e0d69d54bbe52b6bf131ce605ce60efc316e0f
kadoshita <kadoshita.inct@gmail.com>
2018-08-12 18:41:10 +0900

    add skyway connection

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
 $(document).ready(()=>{
-    console.log('test');
+    let peer=null;
+    let room=null;
+
+    $.getJSON('/js/apikey.json',(data)=>{
+        peer=new Peer({
+            key:data.key,
+            debug:3
+        });
+    });
     $('#joinroombtn').on('click',()=>{
         let roomname=$('#roomnameipt').val();
         console.log(roomname);
         if(roomname){
+            room=peer.joinRoom(roomname,{mode:'sfu'});
+            room.on('open',()=>{
+                connect(room);
+            })
             $('#roomnametxt').text(roomname);
             $('#joinroom').toggle();
             $('#chat').toggle();
         }
-    })
+    });
+    $('#leaveroombtn').on('click',()=>{
+        room.close();
+        peer=null;
+        room=null;
+        $('#joinroom').toggle();
+        $('#chat').toggle();
+    });
 });
+
+let connect=_room=>{
+    _room.on('data',msg=>{
+        console.log(msg);
+    });
+    _room.on('join',id=>{
+        console.log(id);
+    });
+    _room.on('peerLeave',id=>{
+        console.log(id);
+    });
+}
