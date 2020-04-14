1a649dc30413a7153b095a4226521eb5643b0a45
kadoshita <sublimer.me@gmail.com>
2020-04-14 20:29:59 +0900

    add first simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, TextField, Button } from '@material-ui/core';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Grid container>
-        <Grid item xs={12}></Grid>
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <h1>SkyWay Multi VoiceChat</h1>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField label="Room ID"></TextField>
+        </Grid>
+        <Grid xs={12}>
+          <Button variant="contained" color="primary">Enter Now!</Button>
+        </Grid>
       </Grid>
     </div>
   );
