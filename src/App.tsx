9f1c3b47a1239fad9b6a95478472076ce2558c6a
kadoshita <sublimer.me@gmail.com>
2020-04-14 20:55:32 +0900

    fix ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <Grid container spacing={4}>
+      <Grid container>
         <Grid item xs={12}>
           <h1>SkyWay Multi VoiceChat</h1>
         </Grid>
         <Grid item xs={12}>
           <TextField label="Room ID"></TextField>
         </Grid>
-        <Grid xs={12}>
+        <Grid item xs={12}>
           <Button variant="contained" color="primary">Enter Now!</Button>
         </Grid>
       </Grid>
