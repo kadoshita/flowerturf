d21e001167e6154392069a9bd7567caf1c7ad603
kadoshita <sublimer.me@gmail.com>
2020-04-15 22:04:58 +0900

    add chat page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router';
 import Home from './Home';
+import Chat from './Chat';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Route exact path="/" component={Home}></Route>
+      <Route path="/chat" component={Chat}></Route>
     </div>
   );
 };
---
+++ src/Chat.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+const Chat = () => {
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <h1>SkyWay Multi VoiceChat</h1>
+            </Grid>
+        </Grid>
+    )
+};
+
+export default Chat;
