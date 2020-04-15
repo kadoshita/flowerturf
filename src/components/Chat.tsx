934814b998ead15836a7bd57e2ac9fd462a5a588
kadoshita <sublimer.me@gmail.com>
2020-04-15 22:51:30 +0900

    add sample user list

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -8,9 +8,7 @@ const Chat = () => {
             <Grid item xs={12}>
                 <h1>SkyWay Multi VoiceChat</h1>
             </Grid>
-            <Grid item xs={2}>
-                <User name='user1'></User>
-            </Grid>
+            {['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10', 'user11', 'user12'].map((u, i) => <Grid item xs={2} key={i}><User name={u}></User></Grid>)}
         </Grid>
     )
 };
