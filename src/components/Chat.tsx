0cc2f95439f9e367481f4a5c02e5112dcff9168f
kadoshita <sublimer.me@gmail.com>
2020-04-15 22:43:25 +0900

    add User component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import User from './User';
 
 const Chat = () => {
     return (
         <Grid container>
             <Grid item xs={12}>
                 <h1>SkyWay Multi VoiceChat</h1>
+            </Grid>
+            <Grid item xs={2}>
+                <User></User>
             </Grid>
         </Grid>
     )
---
+++ src/components/User.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+const User = () => {
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <h1>User1</h1>
+            </Grid>
+        </Grid>
+    )
+};
+
+export default User;
