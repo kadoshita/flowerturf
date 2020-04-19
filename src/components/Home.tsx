3c883f249ce19da2acb5606a8edf980bf9e66d05
kadoshita <sublimer.me@gmail.com>
2020-04-19 17:17:18 +0900

    add username roomname store

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Grid, TextField, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
+import store from '../store/index';
+import { USER_NAME_STORE, ROOM_NAME_STORE } from '../actions/index';
+
 const Home = () => {
     return (
         <Grid container>
@@ -9,10 +12,10 @@ const Home = () => {
                 <h1>SkyWay Multi VoiceChat</h1>
             </Grid>
             <Grid item xs={12}>
-                <TextField label="Room ID" color="primary" style={{ width: '20%' }}></TextField>
+                <TextField label="Room ID" color="primary" style={{ width: '20%' }} onChange={e => store.dispatch({ type: ROOM_NAME_STORE, name: e.target.value })}></TextField>
             </Grid>
             <Grid item xs={12}>
-                <TextField label="User Name" color="primary" style={{ width: '20%' }}></TextField>
+                <TextField label="User Name" color="primary" style={{ width: '20%' }} onChange={e => store.dispatch({ type: USER_NAME_STORE, name: e.target.value })}></TextField>
             </Grid>
             <Grid item xs={12}>
                 <Link to='/chat'>
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -4,11 +4,13 @@ interface IAction {
     type: string,
     name: string
 };
-export default (state = '', action: IAction) => {
+export default (state = { username: '', roomname: '' }, action: IAction) => {
     switch (action.type) {
         case ROOM_NAME_STORE:
+            state.roomname = action.name;
             return state;
         case USER_NAME_STORE:
+            state.username = action.name;
             return state;
         default:
             return state;
