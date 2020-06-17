49f0a473e3cbf069b5b2ed81ddfd7f24f1089314
kadoshita <sublimer.me@gmail.com>
2020-06-17 22:05:17 +0900

    add text input ui

--- src/components/TextChat.tsx
+++ src/components/TextChat.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import { Button, Grid, List, ListItem, ListItemText, Input } from '@material-ui/core';
 
 const TextChat = () => {
     return (
-        <List>
-            <ListItem>
-                <ListItemText
-                    primary='user1'
-                    secondary='sample message'
-                ></ListItemText>
-            </ListItem>
-            <ListItem>
-                <ListItemText
-                    primary='user2'
-                    secondary='sample message2'
-                ></ListItemText>
-            </ListItem>
-        </List>
+        <Grid container spacing={4}>
+            <Grid item xs={12}>
+                <List>
+                    <ListItem>
+                        <ListItemText
+                            primary='user1'
+                            secondary='sample message'
+                        ></ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary='user2'
+                            secondary='sample message2'
+                        ></ListItemText>
+                    </ListItem>
+                </List>
+            </Grid>
+            <Grid item xs={10}>
+                <Input fullWidth></Input>
+            </Grid>
+            <Grid item xs={2}>
+                <Button fullWidth color='primary' variant='contained'>送信</Button>
+            </Grid>
+        </Grid>
     );
 };
 
