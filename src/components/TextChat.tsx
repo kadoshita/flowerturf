1be1fc29274e8302ab7546c15379649c695295e4
kadoshita <sublimer.me@gmail.com>
2020-06-20 22:42:46 +0900

    update scroll to latest chat message

--- src/components/TextChat.tsx
+++ src/components/TextChat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Button, Grid, List, ListItem, ListItemText, Input, Divider } from '@material-ui/core';
 
 type ChatMessage = {
@@ -12,13 +12,34 @@ type TextChatProps = {
 
 const TextChat = (props: TextChatProps) => {
     const [sendMessage, setSendMessage] = useState('');
+    const chatMessagesCount = props.chatMessages.length - 1;
+    const chatMessagesLastItem = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (chatMessagesLastItem) {
+            if (chatMessagesLastItem.current) {
+                chatMessagesLastItem.current.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    }, [props.chatMessages]);
     return (
         <Grid container style={{ height: '100%', paddingLeft: '8px', paddingRight: '8px' }}>
             <Grid item xs={12} style={{ overflowY: 'scroll', height: '90%' }}>
                 <List>
                     {props.chatMessages.map((msg, i) => {
+                        if (i === chatMessagesCount) {
+                            return (
+                                <ListItem key={i} divider>
+                                    <ListItemText
+                                        primary={msg.user}
+                                        secondary={msg.message}
+                                        ref={chatMessagesLastItem}
+                                    ></ListItemText>
+                                </ListItem>
+                            );
+                        }
                         return (
-                            <ListItem key={i}>
+                            <ListItem key={i} divider>
                                 <ListItemText
                                     primary={msg.user}
                                     secondary={msg.message}></ListItemText>
