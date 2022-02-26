785ba7847b8f66f4dffd5800991c9aadc171bebf
kadoshita <sublimer.me@gmail.com>
2022-02-26 18:59:10 +0900

    Add page title

--- pages/app/[name].tsx
+++ pages/app/[name].tsx
@@ -1,7 +1,11 @@
+import { Grid } from '@mui/material';
+import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import MenuAppBar from '../../components/model/appBar';
+import TextChat from '../../components/model/textChat';
 import { RootState } from '../../store';
 
 const Chat = dynamic(() => import('../../components/model/chat'), { ssr: false });
@@ -17,7 +21,24 @@ const App = () => {
     }
   }, []);
 
-  return <Chat></Chat>;
+  return (
+    <div>
+      <Head>
+        <title>FlowerTurf {roomName}</title>
+        <meta name="description" content="複数人対応、インストール不要のボイスチャットツール - FlowerTurf" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MenuAppBar roomName={roomName} userName={userName}></MenuAppBar>
+      <Grid container style={{ height: 'calc(100vmin - 64px)' }}>
+        <Grid item xs={10}>
+          <Chat></Chat>
+        </Grid>
+        <Grid item xs={2} style={{ backgroundColor: 'white' }}>
+          <TextChat></TextChat>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
