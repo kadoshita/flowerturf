9b51640efa1f6679a5c149c73dc5a51928d9e24a
kadoshita <sublimer.me@gmail.com>
2022-02-28 21:10:40 +0900

    Add redux persist

--- store/index.ts
+++ store/index.ts
@@ -1,21 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import deviceReducer from './device';
 import roomReducer from './room';
 import userReducer from './user';
 import chatReducer from './chat';
 import streamReducer from './stream';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const combinedReducers = combineReducers({
+  device: deviceReducer,
+  room: roomReducer,
+  user: userReducer,
+  chat: chatReducer,
+  stream: streamReducer
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['device', 'user']
+};
+
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
 export const store = configureStore({
-  reducer: {
-    device: deviceReducer,
-    room: roomReducer,
-    user: userReducer,
-    chat: chatReducer,
-    stream: streamReducer
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: {
-      ignoredActions: ['stream/updateScreenStream'],
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, 'stream/updateScreenStream'],
       ignoredPaths: ['stream.screen.stream']
     }
   })
@@ -23,3 +34,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
