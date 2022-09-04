cffc8ccc4032f0f78a916c8973697ab4a0319945
kadoshita <sublimer.me@gmail.com>
2022-09-04 10:29:47 +0900

    Update domain

--- components/model/header.tsx
+++ components/model/header.tsx
@@ -14,15 +14,15 @@ const Header = (props: HeaderProps) => {
 
       <meta property="og:title" content="FlowerTurf" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://flowerturf.tk" />
-      <meta property="og:image" content="https://flowerturf.tk/logo.png" />
+      <meta property="og:url" content="https://flowerturf.sublimer.me" />
+      <meta property="og:image" content="https://flowerturf.sublimer.me/logo.png" />
       <meta property="og:description" content="複数人対応、インストール不要のボイスチャットツール - FlowerTurf" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@lz650sss" />
       <meta name="twitter:title" content="FlowerTurf" />
       <meta name="twitter:description" content="複数人対応、インストール不要のボイスチャットツール - FlowerTurf" />
-      <meta name="twitter:image" content="https://flowerturf.tk/logo.png" />
-      <meta name="twitter:url" content="https://flowerturf.tk" />
+      <meta name="twitter:image" content="https://flowerturf.sublimer.me/logo.png" />
+      <meta name="twitter:url" content="https://flowerturf.sublimer.me" />
     </Head>
   );
 };
